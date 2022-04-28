594a08c8c487add7efc0139a3f9d2fe8949f5979
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-28 03:25:29 -0300

    Example 1 Action

--- Predicate/Program.cs
+++ Predicate/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Predicate.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,25 @@ namespace Predicate
     {
         static void Main(string[] args)
         {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Action<Product> act = UpdatePrice;
+
+
+            list.ForEach(act);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+            Console.ReadKey();
+        }
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
         }
     }
 }
