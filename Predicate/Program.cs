e6ac4af96a3a5dab28d94fa384b5025aa31dc4b7
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-28 03:28:12 -0300

    Lambda inline

--- Predicate/Program.cs
+++ Predicate/Program.cs
@@ -17,10 +17,9 @@ namespace Predicate
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Action<Product> act = UpdatePrice;
 
 
-            list.ForEach(act);
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
