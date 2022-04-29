2f3448bad7fbf856a4c6babbfbeb628af8deaf0a
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-29 02:35:21 -0300

    Good Solution

--- Predicate/Program.cs
+++ Predicate/Program.cs
@@ -16,7 +16,7 @@ namespace Predicate
             list.Add(new Product("Mouse", 50.00));
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
-            Func<Product, string> func = NameUpper;
+            Func<Product, string> func = p => p.Name.ToUpper();
 
             List<string> result = list.Select(func).ToList();
             foreach (var item in result)
@@ -24,10 +24,8 @@ namespace Predicate
                 Console.WriteLine(item);
             }
             Console.ReadKey();
+
         }
-        static string NameUpper(Product p)
-        {
-            return p.Name.ToUpper();
-        }
+ 
     }
 }
